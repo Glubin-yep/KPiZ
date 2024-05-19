ec57795a1912757a0aca9c4d1d4e82c80bc8988d
Dmytro Chyr <dima543439@gmail.com>
2024-05-19 19:34:48 +0300

    Fix Todo Model

--- app/src/main/java/com/example/kpiz/models/TODO_Model.kt
+++ app/src/main/java/com/example/kpiz/models/TODO_Model.kt
@@ -4,12 +4,10 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-class TODO_Model {
-    @Entity(tableName = "todo_table")
-    data class Todo(
-        @PrimaryKey(autoGenerate = true) val id: Int?,
-        @ColumnInfo(name = "title") val title: String?,
-        @ColumnInfo(name = "note") val note: String?,
-        @ColumnInfo(name = "date") val date: String
-    ): java.io.Serializable
-}
+@Entity(tableName = "todo_table")
+data class TODO_Model(
+    @PrimaryKey(autoGenerate = true) val id: Int?,
+    @ColumnInfo(name = "title") val title: String?,
+    @ColumnInfo(name = "note") val note: String?,
+    @ColumnInfo(name = "date") val date: String
+): java.io.Serializable
